8295d55c3b03efaddf5efff5eb054622fbc08496
Dennis Kritchko <dennis_kritchko@sfu.ca>
2025-08-05 16:03:21 -0700

    rebased from previous repository

---
+++ build/CMakeFiles/Prometheus.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Prometheus.dir/src/main.mm.o"
+  "CMakeFiles/Prometheus.dir/src/main.mm.o.d"
+  "Prometheus"
+  "Prometheus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang OBJCXX)
+  include(CMakeFiles/Prometheus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
